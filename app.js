7cc2adbe77c7f2d481427f50b335d36debc2f305
Cesar <cesdia883@gmail.com>
2020-10-10 16:49:52 -0400

    figuring out redirects and req.body

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var exphs = require('express-handlebars')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var exphs = require("express-handlebars");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.engine('handlebars', exphs());
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", {
+        actionURL: "https://api.spoonacular.com/recipes/complexSearch",
+    });
+});
+
+router.post("/", function (req, res, next) {
+    console.log("Body: ", req.body);
+    res.redirect([200],"/")
 });
 
 module.exports = router;
