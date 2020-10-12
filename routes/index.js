d4e2c6d4399a73c64a3598e5cc3ecdc79cfc1cfa
Cesar <cesdia883@gmail.com>
2020-10-12 15:05:12 -0400

    simple spoonacular requests

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
-// get the home page
+// GET Display Form
 router.get("/", (req, res, next) => {
     res.render("index");
 });
 
+// POST Submit Form & Display Response
 router.post("/submit", (req, res, next) => {
-    res.send(req.body)
+    let urlString = Object.keys(req.body)
+        .map((key) => `${key}=${req.body[key]}`)
+        .join("&");
+    console.log(`${process.env.SPOON}?${urlString}`);
+    axios
+        .get(`${process.env.SPOON}?${urlString}`)
+        .then((response) => res.json(response.data))
+        .catch((err) => console.log(err));
 });
 
 module.exports = router;
