960f024a9abe358217db6e59910c3c38b3a3082d
Cesar <cesdia883@gmail.com>
2020-10-14 13:57:23 -0400

    handlebars partials added, reformatting styles

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var exphs = require("express-handlebars");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 
-var dotenv = require("dotenv")
-dotenv.config()
+var dotenv = require("dotenv");
+dotenv.config();
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.engine("handlebars", exphs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.engine(
+    "handlebars",
+    exphs({
+        defaultLayout: "main",
+        layoutsDir: __dirname + "/views/layouts",
+        partialsDir: __dirname + "/views/partials",
+    })
+);
 
-app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()) // parse application/json
-app.use(logger(":method :url :status :res[content-length] - :response-time ms"));
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(
+    logger(":method :url :status :res[content-length] - :response-time ms")
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
