746911f1e9565c892d58ecda37e2bc961742c929
Cesar <cesdia883@gmail.com>
2020-10-14 22:21:16 -0400

    preferences using cookies

--- public/javascripts/toggleSettings.js
+++
@@ -1,6 +0,0 @@
-let toggleSettings = () => {
-    let button = document.getElementById("settings-form");
-    button.style.display == "none"
-        ? (button.style.display = "block")
-        : (button.style.display = "none");
-};
